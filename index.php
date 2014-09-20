260421641e48c69b5837e47b9928fb42ad27a894
Grant Nelson <grantmnelsn@gmail.com>
2014-09-19 22:30:50 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>Cimply</title>
+	
+	<!-- Load CSS first (faster) -->
+	<link rel="stylesheet" href="cimply.css">
+	<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.css" />
+
+
+	<!-- Jquery Basic -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	
+	<!-- Jquery Mobile (these need to be minified past MVP) -->
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.js"></script>
+	
+	<!-- Cimply JS -->
+	
+
+</head>
+<body>
+
+
+
+</body>
+</html>
